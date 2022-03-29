f33c8dd2f637b275b1fabb2145899cb8f49e62c6
RamaswamyR <RamaswamyR@LP-CL5GXR2>
2022-03-29 19:50:47 +0100

    Add project files.

---
+++ AareonTechnicalTestTests/Controllers/TicketsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AareonTechnicalTest.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AareonTechnicalTest.Controllers.Tests
+{
+    [TestClass()]
+    public class TicketsControllerTests
+    {
+        [TestMethod()]
+        public void GetTicketsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
